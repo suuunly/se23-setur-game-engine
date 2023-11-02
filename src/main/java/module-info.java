89399f392c1553ec41220afdbe321a7237bed584
flowcore-platform <gitops@flowcore.io>
2023-11-02 21:28:46 +0000

    refactor: cleaned up initial boilerplate

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,10 @@
 module com.setur.se23.gameengine.gameengineboilerplate {
     requires javafx.controls;
     requires javafx.fxml;
-
-
-    opens com.setur.se23.games to javafx.fxml;
-    exports com.setur.se23.games;
     exports com.setur.se23.engine.render;
     opens com.setur.se23.engine.render to javafx.fxml;
     exports com.setur.se23.engine.render.common;
     opens com.setur.se23.engine.render.common to javafx.fxml;
+    exports com.setur.se23;
+    opens com.setur.se23 to javafx.fxml;
 }

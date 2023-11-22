bf627bb77094e84c90cb3c4d0d4d96cdce6536fd
flowcore-platform <gitops@flowcore.io>
2023-11-22 23:20:49 +0000

    refactor: added media library for audio

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.setur.se23.gameengine.gameengineboilerplate {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
     exports com.setur.se23.engine.render;
     opens com.setur.se23.engine.render to javafx.fxml;
     exports com.setur.se23.engine.render.common;

e6fe62561a30801cfb643d73e2b8728854974283
flowcore-platform <gitops@flowcore.io>
2023-11-23 12:02:50 +0000

    fix: fixed failing tests

--- src/test/java/com/setur/se23/engine/game/timing/TimeTest.java
+++ src/test/java/com/setur/se23/engine/game/timing/TimeTest.java
@@ -83,11 +83,6 @@ public class TimeTest {
         assertEquals(0.0f, Time.deltaTime());
     }
 
-    @Test()
-    void test_should_fail() {
-        assertEquals(1, 2);
-    }
-
     @AfterEach
     void tearDown() {
         Time.Destroy();
